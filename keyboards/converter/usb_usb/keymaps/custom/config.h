d3f63ab93cfcbb4af5bde0df36765611c60a3811
fevenor <fevenor@outlook.com>
2019-04-07 10:58:11 +0800

    custom config

--- keyboards/converter/usb_usb/keymaps/custom/config.h
+++ keyboards/converter/usb_usb/keymaps/custom/config.h
@@ -3,4 +3,21 @@
 
 #include "../../config.h"
 
+/* USB Device descriptor parameter */
+
+#undef VENDOR_ID
+#undef PRODUCT_ID
+#undef DEVICE_VER
+#undef MANUFACTURER
+#undef PRODUCT
+#undef DESCRIPTION
+
+#define VENDOR_ID       0xFEED
+#define PRODUCT_ID      0x6666
+#define DEVICE_VER      0x0001
+#define MANUFACTURER    WM
+#define PRODUCT         QMK Keyboard
+#define DESCRIPTION     Keyboard with QMK
+
+
 #endif
